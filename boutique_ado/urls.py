8f2fdbdef178c3e60f8debcda4d75202b67fd7be
tstyer <travisstyer.ts@gmail.com>
2025-11-10 13:59:26 +0000

    Add settings, test email, authenticate

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts', include('allauth.urls')), # Called 'accounts', includes allaugh.urls accounts. (self-learn)
+    path('accounts/', include('allauth.urls')), # Called 'accounts', includes allaugh.urls accounts. (self-learn)
 ]
