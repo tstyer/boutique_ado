df86910ae756b0d620233c76943b382977aa1c3e
tstyer <travisstyer.ts@gmail.com>
2025-11-17 15:23:15 +0000

    Amend settings, urls, views

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')), # Called 'accounts', includes allaugh.urls accounts. (self-learn)
+    path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
 ]
